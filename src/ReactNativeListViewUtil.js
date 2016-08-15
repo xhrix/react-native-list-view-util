37dc003c4912624f20de611b86090d0f8fd23d37
cristian <cristian@gafa.mx>
2016-08-15 02:02:06 -0500

    Remove dependency of linqjs instead we use linqts that is compatible with iPhone

--- src/ReactNativeListViewUtil.js
+++ src/ReactNativeListViewUtil.js
@@ -2,6 +2,8 @@
  * @author xhrix <xxxhrixxx@gmail.com>
  */
 
+import {List} from 'linqts';
+
 /**
  * Utility functions for the 'ListView' component of React Native.
  *
@@ -20,16 +22,12 @@ export default class ReactNativeListViewUtil {
      * @param getSectionData Function to select what data to use as the section data.
      * @param getRowData Function to select what data to use as the row data.
      * @param grouper Function to group the elements of 'dataCollection'.
-     * @param comparer (optional) Function to compare the grouping data.
      * @return {{dataBlob: {}, sectionIDs: Array, rowIDs: Array}}
      */
-    static getDataBlobSectionIDsAndRowIDs<T, TGrouping, TSectionData, TRowData>(
-        dataCollection: T[],
-        getSectionData: (entry: T) => TSectionData,
-        getRowData: (entry: T) => TRowData,
-        grouper: (entry: T) => TGrouping,
-        comparer: (a: TGrouping, b: TGrouping) => boolean = null
-    ) {
+    static getDataBlobSectionIDsAndRowIDs<T, TGrouping, TSectionData, TRowData>(dataCollection: T[],
+                                                                                getSectionData: (entry: T) => TSectionData,
+                                                                                getRowData: (entry: T) => TRowData,
+                                                                                grouper: (entry: T) => TGrouping) {
 
         /**
          * Object containing the contents for all the sections and rows, identified by an id.
@@ -39,7 +37,7 @@ export default class ReactNativeListViewUtil {
 
         /**
          *  Uni-dimensional array with the ids of the section data in the 'dataBlob'.
-         * @type {Array}
+         * @type {[]}
          */
         var sectionIDs = [];
 
@@ -47,46 +45,51 @@ export default class ReactNativeListViewUtil {
          *  Bi-dimensional array with the ids of the row data in the 'dataBlob'.
          *      The first level of the array corresponds to a 'sectionID'.
          *      The second level of the array corresponds to the 'rowIDs'.
-         * @type {Array}
+         * @type {[][]}
          */
         var rowIDs = [];
 
         // First lets group the data so we can get the 'sticky headers' (or sections).
-        let groupedData = dataCollection.groupBy(grouper, comparer);
+        let groupedData = new List(dataCollection).GroupBy(grouper, x=>x);
 
-        var groupIndex = -1;
+        var groupCounter = -1;
 
-        for (var group of groupedData) {
+        for (var groupKey in groupedData) {
 
-            ++groupIndex;
+            if (!groupedData.hasOwnProperty(groupKey)) continue;
 
-            // Add the section ID, using the group index (int) as ID of the section.
-            sectionIDs.push(groupIndex);
+            ++groupCounter;
+
+            let group = groupedData[groupKey];
+
+            // Add the section ID, using the group counter (int) as ID of the section.
+            sectionIDs.push(groupCounter);
 
             // Prepare the 2D array of row IDs.
-            rowIDs[groupIndex] = [];
+            rowIDs[groupCounter] = [];
 
             // Update the data blob with the sticky header (or section) content.
             // All the guys of the group will return the same value when calling 'getSectionData'. Pck the first one.
-            dataBlob[groupIndex] = getSectionData(group[0]);
+            dataBlob[groupCounter] = getSectionData(group[0]);
 
-            var entryIndex = -1;
+            var entryCounter = -1;
 
             // Loop through all the elements in the group.
             for (var entry of group) {
 
-                ++entryIndex;
+                ++entryCounter;
 
                 // Create the row's ID, which is just a string combining the section's ID where the row lives, a comma,
                 // and the index of the row inside the group, this way we ensure this ID is unique though all the
                 // 'dataBlob'.
-                let rowID = `${groupIndex},${entryIndex}`;
-                rowIDs[groupIndex].push(rowID);
+                let rowID = `${groupCounter},${entryCounter}`;
+                rowIDs[groupCounter].push(rowID);
 
                 // Update the data blog with the row's content.
                 dataBlob[rowID] = getRowData(entry);
             }
         }
+
         return {dataBlob: dataBlob, sectionIDs: sectionIDs, rowIDs: rowIDs};
     }
 }
